c9d4263a3e143a87ab2733d108b5a6a5f1169bbf
Kyle Geske <stungeye@gmail.com>
2018-02-07 17:26:34 -0600

    Implemented the index, show, new and create actions.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,39 @@
 class ProductsController < ApplicationController
+  # GET /products
+  # Should load and then display all products.
   def index
+    @products = Product.all
   end
+  # Automatically load up app/views/products/index.html.erb
 
+  # GET /products/:id
+  # Should load the product with the specified id.
   def show
+    @product = Product.find(params[:id])
   end
+  # Automatically load up app/views/products/show.html.erb
 
+  # GET /products/new
+  # Should display a new product form that POSTs to create.
   def new
+    @product = Product.new
   end
+  # Automatically load up app/views/products/new.html.erb
 
   def create
+    @product = Product.new(product_params)
+    if @product.save # Is the product valid?
+      # Product was saved because it is valid.
+      redirect_to product_path(@product) # Load the show action for this new product
+    else
+      # Validation errors!
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :stock_quantity)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  # GET /products     => products controller's index action
+  get 'products', to: 'products#index', as: 'products'
 
+  # GET /products/:id => products controller's show action
+  get 'products/:id', to: 'products#show', as: 'product', id: /\d+/
+
+  # GET /products/new => products controller's new action
+  get 'products/new', to: 'products#new', as: 'new_product'
+
+  # POST /products    => products controller's create action
+  post 'products', to: 'products#create'
 end
